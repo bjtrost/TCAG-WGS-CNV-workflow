985a7246c3a4c292cdf96c5100484a2d161216af
Brett <brett.trost@sickkids.ca>
2017-10-04 20:43:37 -0400

    Generalized

--- reproduce_results.sh
+++ reproduce_results.sh
@@ -1,28 +1,41 @@
 #!/usr/bin/env bash
 
+# Directory $test_data_dir should have contents of supplementary files (File_S1.txt, *.HuRef.*, and RLCRs.txt)
+
+output_dir="reproduce_results_out"
+mkdir $output_dir
+
+test_data_dir="test_data"
+
 # Convert raw output files to common format
-./convert_CNV_calls_to_common_format.py test_data/89931_500.HAS.Canvas.vcf Canvas > Canvas.txt
-./convert_CNV_calls_to_common_format.py test_data/89931_500.cn.MOPS.txt cn.MOPS > cn.MOPS.txt
-./convert_CNV_calls_to_common_format.py test_data/89931_500.CNVnator.bin500.txt CNVnator > CNVnator.txt
-./convert_CNV_calls_to_common_format.py test_data/89931_500.erds.del200.vcf ERDS > ERDS.txt
-./convert_CNV_calls_to_common_format.py test_data/89931_500.GenomeSTRiP.CNVpipeline.vcf Genome_STRiP > Genome_STRiP.tmp
-./convert_CNV_calls_to_common_format.py test_data/89931_500.RDX.txt RDXplorer > RDXplorer.txt
+./convert_CNV_calls_to_common_format.py $test_data_dir/Canvas.HuRef.vcf Canvas > $output_dir/Canvas.txt
+./convert_CNV_calls_to_common_format.py $test_data_dir/cn.MOPS.HuRef.txt cn.MOPS > $output_dir/cn.MOPS.txt
+./convert_CNV_calls_to_common_format.py $test_data_dir/CNVnator.HuRef.txt CNVnator > $output_dir/CNVnator.txt
+./convert_CNV_calls_to_common_format.py $test_data_dir/ERDS.HuRef.vcf ERDS > $output_dir/ERDS.txt
+./convert_CNV_calls_to_common_format.py $test_data_dir/Genome_STRiP.HuRef.vcf Genome_STRiP > $output_dir/Genome_STRiP.tmp
+./convert_CNV_calls_to_common_format.py $test_data_dir/RDXplorer.HuRef.txt RDXplorer > $output_dir/RDXplorer.txt
 
 # Merge overlapping Genome STRiP calls
-./merge_Genome_STRiP.py Genome_STRiP.tmp > Genome_STRiP.txt
-rm Genome_STRiP.tmp
+./merge_Genome_STRiP.py $output_dir/Genome_STRiP.tmp > $output_dir/Genome_STRiP.txt
+rm $output_dir/Genome_STRiP.tmp
 
 # Compare each algorithm against HuRef benchmark
 for algorithm in Canvas cn.MOPS CNVnator ERDS Genome_STRiP RDXplorer; do
-    ./compare_CNVs_to_benchmark.py $algorithm.txt ../supplemental_files/File_S1.txt > $algorithm.txt.bm
+    ./compare_CNVs_to_benchmark.py $output_dir/$algorithm.txt $test_data_dir/File_S1.txt > $output_dir/$algorithm.txt.bm
 done
 
 # Compare benchmark-compared files against RLCR definition
-./compare_with_RLCR_definition.py test_data/RLCRs.txt Canvas.txt.bm cn.MOPS.txt.bm CNVnator.txt.bm ERDS.txt.bm Genome_STRiP.txt.bm RDXplorer.txt.bm
+./compare_with_RLCR_definition.py $test_data_dir/RLCRs.txt $output_dir/Canvas.txt.bm $output_dir/cn.MOPS.txt.bm $output_dir/CNVnator.txt.bm $output_dir/ERDS.txt.bm $output_dir/Genome_STRiP.txt.bm $output_dir/RDXplorer.txt.bm
 
 # Make .counts files, which give different categories of overlap with repeat definition and RLCR definition
 for file in *.RLCR; do
-    ./benchmark_overlap_counts.py $file > $file.counts
+    ./benchmark_overlap_counts.py $output_dir/$file > $output_dir/$file.counts
+
+# Make files containing CNVs from individual benchmark methods
+cp $test_data_dir/File_S1.txt $output_dir
+./split_HuRef_benchmark.sh $output_dir/File_S1.txt
 
 # Make file containing overlap of all methods
-./CNV_overlap.py Canvas.txt,cn.MOPS.txt,CNVnator.txt,ERDS.txt,Genome_STRiP.txt,RDXplorer.txt File_S1.txt.Affymetrix6.0,File_S1.txt.Agilent24M,File_S1.txt.Illumina1M,File_S1.txt.NimbleGen42M,File_S1.txt.SplitRead,File_S1.txt.assembly.comparison,File_S1.txt.cg.cnv,File_S1.txt.cg.mp > all_overlap.txt
+cd $output_dir
+../CNV_overlap.py Canvas.txt,cn.MOPS.txt,CNVnator.txt,ERDS.txt,Genome_STRiP.txt,RDXplorer.txt File_S1.txt.Affymetrix6.0,File_S1.txt.Agilent24M,File_S1.txt.Illumina1M,File_S1.txt.NimbleGen42M,File_S1.txt.SplitRead,File_S1.txt.assembly.comparison,File_S1.txt.cg.cnv,File_S1.txt.cg.mp > all_overlap.txt
+cd ..
