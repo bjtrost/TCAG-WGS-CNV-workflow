f98a81de9cd8de53155fab928b80735b2cdc149e
Brett Trost <brett.trost@usask.ca>
2020-03-08 09:24:04 -0400

    Fix CNV_read_depth_checker

--- CNVworkflowlib.py
+++ CNVworkflowlib.py
@@ -1,4 +1,154 @@
+import statistics
 
+def mean(list):
+    try:
+        mean = statistics.mean(list)
+    except statistics.StatisticsError:
+        mean = 0
+    return(mean)
+
+def stdev(list):
+    try:
+        stdev = statistics.stdev(list)
+    except statistics.StatisticsError:
+        stdev = 0
+    return(stdev)
+
+def depth_search(depth_filename, region):
+    doc_file = open(depth_filename)
+    index_file = open(depth_filename + ".idx")
+
+    chrom, lower_limit, upper_limit = parse_region(region)
+
+    mem_read_depth = 0
+    mem_offset_pos = 0
+
+    #Find offset number for seek()
+    index_file.readline()
+    for mline in index_file:
+        mline = mline.split("\t")
+        if chrom == mline[0] and int(mline[1]) <= lower_limit: # Get the offset that is one before the lower limit
+            mem_offset_pos = int(mline[2])
+        elif chrom == mline[0] and lower_limit < int(mline[1]):
+            break
+
+    #In case lower_limit == 1, or lower_limit < first offset bp pos
+    if mem_offset_pos == 0:
+        index_file.seek(0)
+        for mline in index_file:
+            mline = mline.split("\t")
+            if chrom == mline[0]:
+                mem_offset_pos = int(mline[2])
+                break
+
+    depth_list = []
+
+    # Make list of read depths in requested region
+    doc_file.seek(mem_offset_pos)
+    for line in doc_file:
+        docline_chr, docline_position, docline_depth = line.strip().split("\t")
+        docline_chr = docline_chr.replace("chr", "")
+        docline_position = int(docline_position)
+        docline_depth = int(docline_depth)
+        if chrom == docline_chr and (lower_limit <= docline_position <= upper_limit):
+            depth_list.append(docline_depth)
+        elif chrom == docline_chr and upper_limit < docline_position:
+            break
+        elif chrom != docline_chr:
+            break
+
+    return(depth_list)
+
+# Read in a two column file, and make a dictionary where the
+# key is the first column and the value is the second column
+def dict_from_file(filename, delimiter="\t", key_col=0, value_col=1, header=False, value_type="string"):
+    f = file_or_stdin(filename)
+
+    if header:
+        f.readline()
+
+    d = {}
+
+    for line in f:
+        fields = line.split(delimiter)
+        if value_type == "int":
+            d[fields[key_col].rstrip("\n")] = int(fields[value_col].rstrip("\n"))
+        elif value_type == "float":
+            d[fields[key_col].rstrip("\n")] = float(fields[value_col].rstrip("\n"))
+        else:
+            d[fields[key_col].rstrip("\n")] = fields[value_col].rstrip("\n")
+
+    return(d)
+
+def get_depth_lists(chrom, CNV_start, CNV_end, depth_filename, compute_left_left_right_right=False):
+    region_template = "{0}:{1}-{2}"
+
+    chrom_size = dict_from_file(depth_filename + ".chrinfo", header=True, value_type="int")
+
+    if CNV_end < CNV_start:
+        print("Invalid region entered (start coordinate {} is less than end coordinate {}). Please try again.".format(CNV_start, CNV_end))
+        quit()
+    if CNV_start < 1:
+        print("Invalid region entered (start coordinate is less than 1). Please try again.")
+        quit()
+    if chrom not in chrom_size:
+        print("Invalid chromomosome {} entered. Please try again.".format(chrom))
+        quit()
+    if CNV_end > chrom_size[chrom]:
+        CNV_end = chrom_size[chrom]
+        #print("Invalid region entered (end coordinate {} is greater than chromomosome {} size of {:d}). Please try again.".format(CNV_end, chrom, chrom_size[chrom]))
+    if CNV_start > chrom_size[chrom]:
+        return (None, None, None)
+
+    CNV_size = CNV_end - CNV_start + 1
+
+    left_flank_start = max(1, CNV_start - CNV_size)
+    left_flank_end = CNV_start - 1
+    left_flank_size = left_flank_end - left_flank_start + 1
+
+    right_flank_start = CNV_end + 1
+    right_flank_end = min(right_flank_start + CNV_size - 1, chrom_size[chrom])
+    right_flank_size = right_flank_end - right_flank_start + 1
+
+    left_flank_region = region_template.format(chrom, left_flank_start, left_flank_end)
+    CNV_region = region_template.format(chrom, CNV_start, CNV_end)
+    right_flank_region = region_template.format(chrom, right_flank_start, right_flank_end)
+
+    left_flank_depths = depth_search(depth_filename, left_flank_region)
+    CNV_depths = depth_search(depth_filename, CNV_region)
+    right_flank_depths = depth_search(depth_filename, right_flank_region)
+
+    depth_lists = {}
+    depth_lists["left_flank_depths"] = left_flank_depths + [0] * (left_flank_size - len(left_flank_depths))
+    depth_lists["CNV_depths"] = CNV_depths + [0] * (CNV_size - len(CNV_depths))
+    depth_lists["right_flank_depths"] = right_flank_depths + [0] * (right_flank_size - len(right_flank_depths))
+
+    if compute_left_left_right_right:  # For calculating read depth for short reads, if CNV is 251-300, compare with 151-200 and 351-400
+        left_left_flank_start = max(1, CNV_start - 2*CNV_size)
+        left_left_flank_end = max(1, left_flank_start - 1)
+        left_left_flank_size = left_left_flank_end - left_left_flank_start + 1
+
+        right_right_flank_start = min(right_flank_end + 1, chrom_size[chrom])
+        right_right_flank_end = min(right_right_flank_start + CNV_size - 1, chrom_size[chrom])
+        right_right_flank_size = right_right_flank_end - right_right_flank_start + 1
+
+        left_left_flank_region = region_template.format(chrom, left_left_flank_start, left_left_flank_end)
+        right_right_flank_region = region_template.format(chrom, right_right_flank_start, right_right_flank_end)
+
+        left_left_flank_depths = depth_search(depth_filename, left_left_flank_region)
+        right_right_flank_depths = depth_search(depth_filename, right_right_flank_region)
+
+        depth_lists["left_left_flank_depths"] = left_left_flank_depths + [0] * (left_left_flank_size - len(left_left_flank_depths))
+        depth_lists["right_right_flank_depths"] = right_right_flank_depths + [0] * (right_right_flank_size - len(right_right_flank_depths))
+
+    return(depth_lists)
+
+def parse_region(region): # Given a string in the form chr:start-end, return a three-element list containing chr, start, end
+    chrom = region.split(":")[0].replace("chr", "").upper()
+    start = int(region.split(":")[1].split("-")[0])
+    end = int(region.split(":")[1].split("-")[1])
+
+    return(chrom, start, end)
 
 # Get the size bin name for a particular size
 def get_size_bin(size):
--- get_normalized_depth.py
+++ get_normalized_depth.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 import CNVworkflowlib
@@ -15,23 +15,41 @@ comprising that interval)
 parser = argparse.ArgumentParser(description=description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("depth_filename", type=str, help="filename of samtools depth file", default=None)
 parser.add_argument("region", type=str, help="Region to find the depth of", default=None)
+parser.add_argument("-o", "--generate-outer-flank-ratio", action="store_true", default=False)
 args = parser.parse_args()
 #####################################
 
-chr, start, end = CNVworkflowlib.parse_region(args.region.replace(",", ""))
-left_flank_depths, CNV_depths, right_flank_depths = CNVworkflowlib.get_depth_lists(chr, start, end, args.depth_filename)
-both_flanks_depths = left_flank_depths + right_flank_depths
-
-flanks_mean = CNVworkflowlib.mean(both_flanks_depths)
-flanks_stdev = CNVworkflowlib.stdev(both_flanks_depths)
-
-CNV_mean = CNVworkflowlib.mean(CNV_depths)
-CNV_stdev = CNVworkflowlib.stdev(CNV_depths)
-
+chrom, start, end = CNVworkflowlib.parse_region(args.region.replace(",", ""))
+chrom = chrom.replace("chr", "")
 avg_chromosome_depth = CNVworkflowlib.dict_from_file(args.depth_filename + ".chrinfo", header=True, value_type="float", value_col=2)
 
-normalized_CNV_depth = CNV_mean / avg_chromosome_depth[chr]
-normalized_flanking_depth = flanks_mean / avg_chromosome_depth[chr]
+depth_lists = CNVworkflowlib.get_depth_lists(chrom, start, end, args.depth_filename, compute_left_left_right_right=args.generate_outer_flank_ratio)
+
+
+if args.generate_outer_flank_ratio:
+    both_outer_flanks_depths = depth_lists["left_left_flank_depths"] + depth_lists["right_right_flank_depths"]
+    outer_flanks_mean = CNVworkflowlib.mean(both_outer_flanks_depths)
+    normalized_outer_flanking_depth = outer_flanks_mean / avg_chromosome_depth[chrom]
+
+both_flanks_depths = depth_lists["left_flank_depths"] + depth_lists["right_flank_depths"]
+flanks_mean = CNVworkflowlib.mean(both_flanks_depths)
+CNV_mean = CNVworkflowlib.mean(depth_lists["CNV_depths"])
+CNV_stdev = CNVworkflowlib.stdev(depth_lists["CNV_depths"])
+flanks_stdev = CNVworkflowlib.stdev(both_flanks_depths)
+
+
+try:
+    normalized_CNV_stdev = CNV_stdev / CNV_mean
+except:
+    normalized_CNV_stdev = 0
+
+try:
+    normalized_flanks_stdev = flanks_stdev / flanks_mean
+except:
+    normalized_flanks_stdev = 0
+
+normalized_CNV_depth = CNV_mean / avg_chromosome_depth[chrom]
+normalized_flanking_depth = flanks_mean / avg_chromosome_depth[chrom]
 
 # Meanings of output columns:
 # column 1: mean read depth of the CNV
@@ -41,8 +59,17 @@ normalized_flanking_depth = flanks_mean / avg_chromosome_depth[chr]
 # column 5: ratio between the mean read depth of the CNV and the mean read depth of the flanking regions (col1/col2)
 # column 6: mean read depth of the chromosome
 
-try:
-    ratio = normalized_CNV_depth / normalized_flanking_depth
-    print("{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}".format(CNV_mean, flanks_mean, normalized_CNV_depth, normalized_flanking_depth, ratio, avg_chromosome_depth[chr]))
-except ZeroDivisionError:
-    print("{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{}\t{:.3f}".format(CNV_mean, flanks_mean, normalized_CNV_depth, normalized_flanking_depth, "N/A", avg_chromosome_depth[chr]))
+
+if args.generate_outer_flank_ratio:
+    try:
+        ratio = normalized_CNV_depth / normalized_flanking_depth
+        outer_ratio = normalized_CNV_depth / normalized_outer_flanking_depth
+        print("{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}".format(CNV_mean, flanks_mean, normalized_CNV_depth, normalized_flanking_depth, ratio, avg_chromosome_depth[chrom], outer_flanks_mean, outer_ratio))
+    except ZeroDivisionError:
+        print("{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{}\t{:.3f}\t{:.3f}\t{}".format(CNV_mean, flanks_mean, normalized_CNV_depth, normalized_flanking_depth, "N/A", avg_chromosome_depth[chrom], outer_flanks_mean, "N/A"))
+else:
+    try:
+        ratio = normalized_CNV_depth / normalized_flanking_depth
+        print("{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}".format(CNV_mean, flanks_mean, normalized_CNV_depth, normalized_flanking_depth, ratio, avg_chromosome_depth[chrom], CNV_stdev, normalized_CNV_stdev, flanks_stdev, normalized_flanks_stdev))
+    except ZeroDivisionError:
+        print("{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}".format(CNV_mean, flanks_mean, normalized_CNV_depth, normalized_flanking_depth, "N/A", avg_chromosome_depth[chrom], CNV_stdev, normalized_CNV_stdev, flanks_stdev, normalized_flanks_stdev))
